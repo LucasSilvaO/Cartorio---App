60f6573c536c441269ce3afe60a761917c38eade
unknown <lucas.oliveiraaa20@gmail.com>
2025-07-05 21:57:05 -0300

    init

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,1 +1,1 @@
-export const API_BACK_URL = 'https://mo84p5ordj.execute-api.sa-east-1.amazonaws.com/dev/api';
+export const API_BACK_URL = 'https://232tqh2q0m.execute-api.sa-east-1.amazonaws.com/prod';
